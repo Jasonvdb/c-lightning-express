31a388df1b83c62dc8a92726726733cd6c7da0e4
Jason van den Berg <jayvdb1@gmail.com>
2018-06-10 09:57:37 +0200

    Added new functions

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,16 @@ var LightningClient = require("lightning-client");
 
 var client = new LightningClient("/root/.lightning", true);
 
+//TODO intercept request
+//Check the API key
+//Check the signed URL matched the url signed with the secret on the server
+
 router.get("/", function(req, res, next) {
 	//TODO list available functions here
 	res.send("Available functions coming soon.");
 });
 
-router.get("/info", function(req, res, next) {
+router.get("/getinfo", function(req, res, next) {
 	client
 		.getinfo()
 		.then(info => {
@@ -20,7 +24,18 @@ router.get("/info", function(req, res, next) {
 		});
 });
 
-router.get("/peers", function(req, res, next) {
+router.get("/listpayments", function(req, res, next) {
+	client
+		.listpayments()
+		.then(result => {
+			res.json({ status: "success", payments: result.payments });
+		})
+		.catch(errorResult => {
+			res.status(400).json({ status: "error", error: errorResult.error });
+		});
+});
+
+router.get("/listpeers", function(req, res, next) {
 	client
 		.listpeers()
 		.then(result => {
@@ -31,15 +46,59 @@ router.get("/peers", function(req, res, next) {
 		});
 });
 
+router.get("/listfunds", function(req, res, next) {
+	client
+		.listfunds()
+		.then(result => {
+			res.json({ status: "success", funds: result });
+		})
+		.catch(errorResult => {
+			res.status(400).json({ status: "error", error: errorResult.error });
+		});
+});
+
 router.get("/invoice", function(req, res, next) {
+	var satoshis = req.query.satoshis;
 	var label = req.query.label;
 	var description = req.query.description;
-	var satoshis = req.query.description;
 
 	client
 		.invoice(satoshis, label, description, 3600)
 		.then(invoice => {
 			res.json({ status: "success", invoice });
+		})
+		.catch(errorResult => {
+			res.status(400).json({ status: "error", error: errorResult.error });
+		});
+});
+
+router.get("/connect", function(req, res, next) {
+	var nodeId = req.query.nodeId;
+	var ipAddress = req.query.ipAddress;
+	var port = req.query.port;
+
+	client
+		.connect(
+			nodeId,
+			ipAddress,
+			port
+		)
+		.then(result => {
+			res.json({ status: "success", result });
+		})
+		.catch(errorResult => {
+			res.status(400).json({ status: "error", error: errorResult.error });
+		});
+});
+
+router.get("/fundchannel", function(req, res, next) {
+	var nodeId = req.query.nodeId;
+	var satoshis = req.query.satoshis;
+
+	client
+		.fundchannel(nodeId, satoshis)
+		.then(result => {
+			res.json({ status: "success", result });
 		})
 		.catch(errorResult => {
 			res.status(400).json({ status: "error", error: errorResult.error });
@@ -60,7 +119,8 @@ router.get("/decodepay", function(req, res, next) {
 });
 
 router.get("/pay", function(req, res, next) {
-	var bolt11 = req.query.bolt11;
+	//Sometimes scans have this in the string, remove it in case
+	var bolt11 = req.query.bolt11.replace("lightning:", "");
 
 	client
 		.pay(bolt11)
