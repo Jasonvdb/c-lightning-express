55f9fd812c4eb6cd7d4b617be2ae4ae381c535f5
Jason van den Berg <jayvdb1@gmail.com>
2018-06-16 14:59:24 +0200

    Updated landing page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PayBolts.com' });
+router.get("/", function(req, res, next) {
+	res.render("index", {
+		title: "c-lightning-express",
+		description: "Lightning network REST API for c-lightning",
+		link: "https://github.com/Jasonvdb/c-lightning-express"
+	});
 });
 
 module.exports = router;
